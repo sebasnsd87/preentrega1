476d86fbcd975924af75063d673a29cb01ff97b3
Sebastian Duarte <sebasnsd13@gmail.com>
2023-08-16 00:17:20 -0300

    miercoles

--- js/contacto.js
+++ js/contacto.js
@@ -1,23 +1,111 @@
 
 // Inicializar emailjs 
 
-const btn = document.getElementById('button');
+
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
+ document.addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Enviando...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_5d3862p';
+   /*const serviceID = 'default_service';
+   const templateID = 'template_5d3862p';*/
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Enviando Email';
-      alert('Enviado!');
+   /*emailjs.sendForm(serviceID, templateID, this)*/
+    /*.then(() => {
+      btnEnviar.value = 'Enviando Email';
+      Swal.fire({
+        title: 'Email enviado correctamente!!!',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
     }, (err) => {
-      btn.value = 'Send Email';
+      btnEnviar.value = 'Send Email';
       alert(JSON.stringify(err));
+    });*/
+  })
+
+document.addEventListener('DOMContentLoaded', function() {
+  const registrationForm = document.getElementById('registration-form');
+  
+  if (registrationForm) {
+    registrationForm.addEventListener('submit', async function(event) {
+      event.preventDefault();
+
+      const nombre = document.getElementById("nombre").value;
+      const email = document.getElementById("email").value;
+      const telefono = document.getElementById("tel").value;
+      const comentario = document.getElementById("comentario").value;
+
+      // Validar campos
+      if (nombre.trim() === "" || email.trim() === "" || telefono.trim() === "" || comentario.trim() === "") {
+        Swal.fire('Error', 'Por favor, completa todos los campos correctamente', 'error');
+        return;
+      }
+
+      // Comprobar si el email ya existe en el LocalStorage
+      if (existeEmailEnLocalStorage(email)) {
+        Swal.fire('Error', 'El email ya ha sido registrado.', 'error');
+        return;
+      }
+
+      // Guardar en LocalStorage
+      guardarEnLocalStorage(nombre, email, telefono, comentario);
+
+      // Inicializar EmailJS con tu ID de usuario
+      emailjs.init("4bbCK443WVaRgg872");
+
+      const serviceID = 'default_service';
+      const templateID = 'template_5d3862p';
+
+      try {
+        const response = await emailjs.sendForm(serviceID, templateID, this);
+        if (response.status === 200) {
+          Swal.fire('Éxito', 'Formulario enviado correctamente', 'success');
+          registrationForm.reset();
+        } else {
+          Swal.fire('Error', 'Hubo un problema al enviar el formulario. Por favor, intenta nuevamente más tarde.', 'error');
+        }
+      } catch (error) {
+        console.error("Error al enviar el formulario:", error);
+        Swal.fire('Error', 'Hubo un problema al enviar el formulario. Por favor, intenta nuevamente más tarde.', 'error');
+      }
     });
+  }
 });
+
+// Función para verificar si el email ya existe en LocalStorage
+function existeEmailEnLocalStorage(email) {
+  const contactosGuardados = obtenerContactosLocalStorage();
+  return contactosGuardados.some(contacto => contacto.email === email);
+}
+
+// Función para guardar el contacto en LocalStorage
+function guardarEnLocalStorage(nombre, email, telefono, comentario) {
+  const nuevoContacto = { nombre, email, telefono, comentario };
+  const contactosGuardados = obtenerContactosLocalStorage();
+  contactosGuardados.push(nuevoContacto);
+  localStorage.setItem("contactos", JSON.stringify(contactosGuardados));
+}
+
+// Función para obtener los contactos almacenados en LocalStorage
+function obtenerContactosLocalStorage() {
+  const contactosJSON = localStorage.getItem("contactos");
+  return contactosJSON ? JSON.parse(contactosJSON) : [];
+}
+
+const btnEnviar = document.getElementById('button');
+
+btnEnviar.addEventListener('click', () => {
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your work has been saved',
+    showConfirmButton: false,
+    timer: 1500
+  })
+})
--- js/modal.js
+++ js/modal.js
@@ -70,3 +70,4 @@ btn2.addEventListener('click', () => {
     }
   })
 })
+
